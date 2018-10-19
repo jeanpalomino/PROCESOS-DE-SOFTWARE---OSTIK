1dd8086d98036d25a7798118068292ca49ae6f96
jeanpalomino <43081786+jeanpalomino@users.noreply.github.com>
2018-10-19 16:03:07 -0500

    Add files via upload

---
+++ OSTIK v2.1/ostikv20/Controller/customer_registration.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title></title>
+		<link rel="stylesheet" href="../View/css/bootstrap.min.css"/>
+		<link rel="stylesheet" href="../View/estilo1.css"/>
+		<script src="js/jquery2.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<script src="main.js"></script>
+	</head>
+ <body>
+	<div class="navbar navbar-inverse navbar-fixed-top">
+		<div class="container-fluid">	
+			<div class="navbar-header">
+				<a class="navbar-brand"><img align="top" src='../View/ostik.jpg' style='width:120px; height:45px'/></a>
+			</div>
+			<ul class="nav navbar-nav">
+				<li><a href="index.php"><span class="glyphicon glyphicon-home"></span>Inicio</a></li>
+				
+			</ul>
+		</div>
+	</div>
+	<p><br/></p>
+	<p><br/></p>
+	<p><br/></p>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-2"></div>
+			<div class="col-md-8" id="signup_msg">
+				<!--Alert from signup form-->
+			</div>
+			<div class="col-md-2"></div>
+		</div>
+		<div class="row"><font color='black'>
+			<div class="col-md-4"></div>
+			<div class="col-md-4">
+				<div class="panel panel-primary">
+					<div class="panel-heading">Formulario de registro de cliente</div>
+					<div class="panel-body">
+					
+					<form method="post">
+						<div class="row">
+							<div class="col-md-6">
+								<label for="f_name">Nombre</label>
+								<input type="text" id="f_name" name="f_name" class="form-control">
+							</div>
+							<div class="col-md-6">
+								<label for="f_name">Apellidos</label>
+								<input type="text" id="l_name" name="l_name" class="form-control">
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-md-12">
+								<label for="email">Email</label>
+								<input type="text" id="email" name="email" class="form-control">
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-md-12">
+								<label for="password">password</label>
+								<input type="password" id="password" name="password" class="form-control">
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-md-12">
+								<label for="repassword">Repetir Password</label>
+								<input type="password" id="repassword" name="repassword" class="form-control">
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-md-12">
+								<label for="mobile">Celular</label>
+								<input type="text" id="mobile" name="mobile" class="form-control">
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-md-12">
+								<label for="address1">Dirección</label>
+								<input type="text" id="address1" name="address1" class="form-control">
+							</div>
+						</div>
+						<p><br/></p>
+						<div class="row">
+							<div class="col-md-12">
+								<input style="float:right;" value="Sign Up" type="button" id="signup_button" name="signup_button"  class="btn btn-success btn-lg">
+							</div>
+						</div>
+						
+					</div>
+					</form>
+				</div>
+			</div>
+			<div class="col-md-2"></div>
+		</div>
+	</div>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
